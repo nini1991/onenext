b9361f26846df2e7aeea92e40f77355f7779001f
NIthin Antony <nithinparathara007@gmail.com>
2018-06-07 01:04:23 +0530

    Initial commit

---
+++ src/main/java/com/nachsterlabs/onenext/OnenextApplication.java
@@ -0,0 +1,12 @@
+package com.nachsterlabs.onenext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnenextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnenextApplication.class, args);
+	}
+}
